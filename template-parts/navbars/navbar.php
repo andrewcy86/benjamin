366c377fe6df5c49a055b983e8e525a437187dab
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-11-06 11:35:05 -0500

    Update navbar.php

--- template-parts/navbars/navbar.php
+++ template-parts/navbars/navbar.php
@@ -1,8 +1,47 @@
 <?php
 $sticky = get_theme_mod('navbar_sticky_setting', 'no') == 'yes' ? 'sticky' : '';
 ?>
-<header>
-
-       REMOVED
-
+<header class="masthead clearfix" role="banner">
+		<img alt="" class="site-logo" src="https://www.epa.gov/sites/all/themes/epa/logo.png">
+		<div class="site-name-and-slogan">
+			<h1 class="site-name"><a href="https://www.epa.gov/" rel="home" title="Go to the home page"><span>US EPA</span></a></h1>
+			<div class="site-slogan">United States Environmental Protection Agency</div>
+		</div>
+		<div class="region-header">
+			<div class="block-epa-core-gsa-epa-search" id="block-epa-core-gsa-epa-search">
+				<form action="https://search.epa.gov/epasearch/epasearch" class="epa-search" method="get">
+					<label class="element-hidden" for="search-box">Search</label>
+          <input class="form-text" id="search-box" name="querytext" placeholder="Search EPA.gov" value="">
+          <button class="epa-search-button" id="search-button" title="Search" type="submit">Search</button>
+          <input name="areaname" type="hidden" value="">
+          <input name="areacontacts" type="hidden" value="">
+          <input name="areasearchurl" type="hidden" value="">
+          <input name="typeofsearch" type="hidden" value="epa">
+          <input name="result_template" type="hidden" value="2col.ftl">
+				</form>
+			</div>
+		</div>
 </header>
+	<nav class="nav main-nav clearfix" role="navigation">
+		<div class="nav__inner">
+			<h2 class="element-invisible">Main menu</h2>
+			<ul class="menu" role="menu">
+				<li class="menu-item" role="presentation"><a class="menu-link" href="https://www.epa.gov/environmental-topics" role="menuitem" title="Learn about Environmental Topics that EPA covers.">Environmental Topics</a></li>
+				<li class="menu-item" role="presentation"><a class="menu-link" href="https://www.epa.gov/laws-regulations" role="menuitem" title="Laws written by Congress provide the authority for EPA to write regulations. Regulations explain the technical, operational, and legal details necessary to implement laws.">Laws &amp; Regulations</a></li>
+				<li class="menu-item" role="presentation"><a class="menu-link" href="https://www.epa.gov/aboutepa" role="menuitem" title="Learn more about our mission and what we do, how we are organized, and our history.">About EPA</a></li>
+			</ul>
+		</div>
+	</nav>
+<div class="mobile-nav" id="mobile-nav">
+		<div class="mobile-bar clearfix">
+			<label class="menu-button" for="mobile-nav-toggle">Menu</label>
+		</div>
+    <input checked id="mobile-nav-toggle" type="checkbox">
+		<div class="mobile-links element-hidden" id="mobile-links" style="height:2404px;">
+			<ul class="mobile-menu">
+				<li class="menu-item" role="presentation"><a class="menu-link" href="https://www.epa.gov/environmental-topics" role="menuitem" title="Learn about Environmental Topics that EPA covers.">Environmental Topics</a></li>
+				<li class="menu-item" role="presentation"><a class="menu-link" href="https://www.epa.gov/laws-regulations" role="menuitem" title="Laws written by Congress provide the authority for EPA to write regulations. Regulations explain the technical, operational, and legal details necessary to implement laws.">Laws &amp; Regulations</a></li>
+				<li class="menu-item" role="presentation"><a class="menu-link" href="https://www.epa.gov/aboutepa" role="menuitem" title="Learn more about our mission and what we do, how we are organized, and our history.">About EPA</a></li>
+			</ul>
+		</div>
+	</div>
