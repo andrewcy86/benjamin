0e7d21f3250a3f6d93d5092b69d892fbc8140255
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-11-06 11:32:39 -0500

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,10 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="//www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-L8ZB");</script>
+  <!-- End Google Tag Manager -->  
+    
 <?php wp_head(); ?>
 </head>
 
