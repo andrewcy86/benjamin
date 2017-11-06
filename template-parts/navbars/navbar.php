336859d49231b4a70cb348f4896a7b3b78fc210c
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-11-06 09:56:46 -0500

    Update navbar.php

--- template-parts/navbars/navbar.php
+++ template-parts/navbars/navbar.php
@@ -1,13 +1,8 @@
 <?php
 $sticky = get_theme_mod('navbar_sticky_setting', 'no') == 'yes' ? 'sticky' : '';
 ?>
-<header class="usa-header usa-header-basic
-    <?php echo esc_attr(benjamin_navbar_header_class()) . ' ' . esc_attr($sticky) ; ?>" role="banner">
-    <div class="usa-nav-container">
+<header>
 
        REMOVED
-
-
-    </div>
 
 </header>
