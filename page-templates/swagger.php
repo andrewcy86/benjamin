1c1885e1c90ae06f8f3821bfc779c4959b768d88
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-11-08 11:59:16 -0500

    Update swagger.php

--- page-templates/swagger.php
+++ page-templates/swagger.php
@@ -52,6 +52,24 @@ if( !$hide_content ):
     endif;
     ?>
     <div class="main-content <?php echo esc_attr($main_width); ?>">
+        <section id="primary" class="usa-grid usa-section">
+
+    <?php
+    if($sidebar_position == 'left'):
+        benjamin_get_sidebar($template, $sidebar_position);
+    endif;
+    ?>
+    <div class="main-content <?php echo esc_attr($main_width); ?>">
+    	<?php
+    	while ( have_posts() ) : the_post();
+
+    		get_template_part( 'template-parts/content', 'page' );
+
+
+    	endwhile; // End of the loop.
+    	?>
+    </div>
+            
     	<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="position:absolute;width:0;height:0">
   <defs>
     <symbol viewBox="0 0 20 20" id="unlocked">
